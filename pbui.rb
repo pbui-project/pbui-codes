abdaab9d2ad2dfb74e0e02673ae6550a7a5879b8
Michael Eisemann <japanese_kitkat@yahoo.co.jp>
2020-01-16 15:05:35 -0500

    working on erb stuff

--- pbui.rb
+++ pbui.rb
@@ -4,19 +4,17 @@ require 'sinatra'
 
 set :port, 7284
 get '/' do
-  sensors = `sensors`
-  hddtemp = `hddtemp`
-  free = `free -h`
-  dfh = `df -h`
-  mpstat = `mpstat`
-  uptime = `uptime`
 
-  erb :index, locals:
-  { sensors_data: `sensors`,
-    hddtemp_data: `hddtemp`,
-    free_data: `free -h`,
-    df_data: `df -h`,
-    mpstat_data: `mpstat`,
-    uptime_data: `uptime` }
+  erb :index, :layout => :layout
+  # erb :index, locals:
+  # { sensors_data: `sensors`,
+  #   hddtemp_data: `hddtemp`,
+  #   free_data: `free -h`,
+  #   df_data: `df -h`,
+  #   mpstat_data: `mpstat`,
+  #   uptime_data: `uptime` }
 end
 
+get '/members' do
+  erb :members
+end
